b140942254c8d08bd3a36c36d08b33dbd5ec8573
bhaskarkumarsinha <sinhakumarbhaskar@gmail.com>
2022-05-25 01:21:11 +0530

    Some Changes to bug

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,15 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import "./index.css";
 import App from "./App";
 import { ContextProvider } from "./contexts/ContextProvider";
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <ContextProvider>
+//       <App />
+//     </ContextProvider>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <ContextProvider>
       <App />
     </ContextProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
